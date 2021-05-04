b3a1991eaea30315c261d42f9253c96d30cd3aa6
Niraj Sapkota <n.sapkota@outlook.com.au>
2021-05-04 10:06:47 +1000

    Add Update and Delete Endpoints

--- api/src/main/java/com/optus/microservice/api/controllers/StudentController.java
+++ api/src/main/java/com/optus/microservice/api/controllers/StudentController.java
@@ -1,11 +1,16 @@
 package com.optus.microservice.api.controllers;
 
+import java.util.Optional;
+
 import com.optus.microservice.api.models.Student;
 import com.optus.microservice.api.repository.StudentRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,13 +21,33 @@ public class StudentController {
 	StudentRepository studentRepository;
 
   @PostMapping("/student")
-  public void createStudent(@RequestBody Student s) {
+  public String createStudent(@RequestBody Student s) {
     studentRepository.save(s);
+    return "Successfully created student " + s.getName();
   }
 
   @GetMapping("/students")
   public Iterable<Student> getStudents() {
     return studentRepository.findAll();
+  }
+
+  @PutMapping("/student/{id}")
+  public String updateStudent(@RequestBody Student s, @PathVariable Long id) {
+    Optional<Student> optDBStudent = studentRepository.findById(id);
+    if (!optDBStudent.isPresent()) {
+      return "Unable to find a student with the specified ID";
+    }
+    
+    Student dbStudent = optDBStudent.get();
+    dbStudent.setName(s.getName());
+    studentRepository.save(dbStudent);
+    return "Successfully updated student";
+  }
+
+  @DeleteMapping("/student/{id}")
+  public String deleteStudent(@PathVariable Long id) {
+    studentRepository.deleteById(id);
+    return "Successfully deleted student";
   }
 
 }
